75920b8ed8c683e341a6a01d03f5889afaeec5ac
neogrammer <jlhardee1986@gmail.com>
2024-08-15 19:59:24 -0400

    update

--- main.cpp
+++ main.cpp
@@ -4,46 +4,46 @@
 #include <iostream>
 #include <string>
 
-
-struct PlayerData
-{
-	int xpos{};
-	int ypos{};
-};
-
-struct BallData
-{
-	int xpos{};
-	int ypos{};
-};
-
-struct FrameData
-{
-	int ypos{};
-};
-
-struct WorldData
-{
-	int yourYPos{};
-	int otherYPos{};
-	int ballXPos{};
-	int ballYPos{};
-};
+//
+//struct PlayerData
+//{
+//	int xpos{};
+//	int ypos{};
+//};
+//
+//struct BallData
+//{
+//	int xpos{};
+//	int ypos{};
+//};
+//
+//struct FrameData
+//{
+//	int ypos{};
+//};
+//
+//struct WorldData
+//{
+//	int yourYPos{};
+//	int otherYPos{};
+//	int ballXPos{};
+//	int ballYPos{};
+//};
 
 int gPort = 55555;
 int numPlayersConnected = 0;
-
-int ballDirX = -1;
-int ballDirY = 1;
-int xSpeed = 0;
-int ySpeed = 0;
-
-PlayerData player1{};
-PlayerData player2{};
-FrameData inData{};
-WorldData outData{};
-BallData ball{};
-
+//
+//int ballDirX = -1;
+//int ballDirY = 1;
+//int xSpeed = 0;
+//int ySpeed = 0;
+//
+//PlayerData player1{};
+//PlayerData player2{};
+//FrameData inData{};
+//WorldData outData{};
+//BallData ball{};
+//
 
 
 void update();
@@ -78,39 +78,63 @@ int main()
 		std::cout << "socket() is OK!" << std::endl;
 	}
 
-	// bind the specifics for the socket with its specific configuration
-	sockaddr_in service;
-	service.sin_family = AF_INET;
-	InetPton(AF_INET, L"127.0.0.1", &service.sin_addr.s_addr);
-	service.sin_port = htons(gPort);
-	if (bind(listener, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
-	{
-		std::cout << "bind failed on listener" << WSAGetLastError() << std::endl;
-		closesocket(listener);
-		WSACleanup();
-		return 0;
-	}
-	else
-	{
-		std::cout << "Socket bound!" << std::endl;
-	}
+   // Get the hostname of the current machine
+char hostname[256];
+if (gethostname(hostname, sizeof(hostname)) != 0) {
+	std::cout << "gethostname failed" << std::endl;
+	WSACleanup();
+	return 0;
+}
 
-	if (listen(listener, 8) == SOCKET_ERROR)
-	{
-		std::cout << "listen(): Error listening on socket! " << WSAGetLastError() << std::endl;
-		closesocket(listener);
-		WSACleanup();
-		return 0;
-	}
-	else
-	{
-		std::cout << "Socket now listening for incoming requests on port: " << gPort << std::endl;
-	}
+// Get the IP address of the current machine
+struct addrinfo hints, * result, * ptr;
+ZeroMemory(&hints, sizeof(hints));
+hints.ai_family = AF_INET;
+hints.ai_socktype = SOCK_STREAM;
+hints.ai_flags = AI_PASSIVE;
 
-	std::cout << "Waiting for client connection" << std::endl;
+if (getaddrinfo(hostname, NULL, &hints, &result) != 0) {
+	std::cout << "getaddrinfo failed" << std::endl;
+	WSACleanup();
+	return 0;
+}
+
+sockaddr_in service;
+for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+	if (ptr->ai_family == AF_INET) {
+		service.sin_family = AF_INET;
+		service.sin_addr = ((struct sockaddr_in*)ptr->ai_addr)->sin_addr;
+		service.sin_port = htons(gPort);
+		break;
+	}
+}
+
+freeaddrinfo(result);
+
+// bind the specifics for the socket with its specific configuration
+if (bind(listener, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+	std::cout << "bind failed on listener" << WSAGetLastError() << std::endl;
+	closesocket(listener);
+	WSACleanup();
+	return 0;
+}
+else {
+	std::cout << "Socket bound!" << std::endl;
+}
+
+if (listen(listener, 8) == SOCKET_ERROR) {
+	std::cout << "listen(): Error listening on socket! " << WSAGetLastError() << std::endl;
+	closesocket(listener);
+	WSACleanup();
+	return 0;
+}
+else {
+	std::cout << "Socket now listening for incoming requests on port: " << gPort << std::endl;
+}
+
+std::cout << "Waiting for client connection" << std::endl;
 
 	SOCKET clientPipeSocket = INVALID_SOCKET;
-	SOCKET clientPipeSocket2 = INVALID_SOCKET;
 	clientPipeSocket = accept(listener, NULL, NULL);
 	if (clientPipeSocket == INVALID_SOCKET)
 	{
@@ -126,11 +150,12 @@ int main()
 		numPlayersConnected++;
 	}
 	
+	SOCKET clientPipeSocket2 = INVALID_SOCKET;
 	clientPipeSocket2 = accept(listener, NULL, NULL);
 	if (clientPipeSocket2 == INVALID_SOCKET)
 	{
 		std::cout << "accept failed with player 2: " << WSAGetLastError() << std::endl;
-		closesocket(clientPipeSocket);
+		closesocket(clientPipeSocket2);
 		closesocket(listener);
 		WSACleanup();
 		return 0;
@@ -181,7 +206,7 @@ int main()
 	// int send ( SOCKET, const char* buf //MESSAGE//, int len //Length of Message Array(individual characters)//, int flags //DEFAULT OK- pass 0//); 
 	//  returns number of bytes sent, non-zero is ideal here
 
-	ball.xpos = 785;
+	/*ball.xpos = 785;
 	ball.ypos = 435;
 
 	xSpeed = 3;
@@ -191,92 +216,92 @@ int main()
 	ballDirY = 1;
 	
 	player1.xpos = 40;
-	player2.xpos = 1510;
+	player2.xpos = 1510;*/
 
 	while (true)
 	{
-		int readBytes = 0;
-		char readBuffer[4];
-		readBytes = recv(clientPipeSocket, readBuffer, 4, 0);
-		if (readBytes == SOCKET_ERROR)
-		{
-			//error
-			goto loopend;
-		}
+		//int readBytes = 0;
+		//char readBuffer[4];
+		//readBytes = recv(clientPipeSocket, readBuffer, 4, 0);
+		//if (readBytes == SOCKET_ERROR)
+		//{
+		//	//error
+		//	goto loopend;
+		//}
 
-		int readBytes2 = 0;
-		char readBuffer2[4];
-		readBytes2 = recv(clientPipeSocket2, readBuffer2, 4, 0);
-		if (readBytes2 == SOCKET_ERROR)
-		{
-			//error
-			goto loopend;
+		//int readBytes2 = 0;
+		//char readBuffer2[4];
+		//readBytes2 = recv(clientPipeSocket2, readBuffer2, 4, 0);
+		//if (readBytes2 == SOCKET_ERROR)
+		//{
+		//	//error
+		//	goto loopend;
 
-		}
+		//}
 
 		// Update world here
 
 
 
-		std::string tmp = "000";
-		tmp.at(0) = readBuffer[0];
-		tmp.at(1) = readBuffer[1];
-		tmp.at(2) = readBuffer[2];
+		//std::string tmp = "000";
+		//tmp.at(0) = readBuffer[0];
+		//tmp.at(1) = readBuffer[1];
+		//tmp.at(2) = readBuffer[2];
 
-		int newYPos = stoi(tmp);
-		player1.ypos = newYPos;
+		//int newYPos = stoi(tmp);
+		//player1.ypos = newYPos;
 
-		std::string tmp2 = "000";
-		tmp2.at(0) = readBuffer2[0];
-		tmp2.at(1) = readBuffer2[1];
-		tmp2.at(2) = readBuffer2[2];
+		//std::string tmp2 = "000";
+		//tmp2.at(0) = readBuffer2[0];
+		//tmp2.at(1) = readBuffer2[1];
+		//tmp2.at(2) = readBuffer2[2];
 
-		int newYPos2 = stoi(tmp2);
-		player2.ypos = newYPos2;
+		//int newYPos2 = stoi(tmp2);
+		//player2.ypos = newYPos2;
 
-		
+		//
 
-		update();
-		std::string ballxStr = "0000";
-		std::string bx = std::to_string(ball.xpos);
-		size_t diff = ballxStr.length() - bx.length();
-		for (size_t i = 0; i < bx.length(); i++)
-		{
-			ballxStr.at(i + diff) =  bx.at(i);
-		}
+		//update();
+		//std::string ballxStr = "0000";
+		//std::string bx = std::to_string(ball.xpos);
+		//size_t diff = ballxStr.length() - bx.length();
+		//for (size_t i = 0; i < bx.length(); i++)
+		//{
+		//	ballxStr.at(i + diff) =  bx.at(i);
+		//}
 
 
-		std::string ballyStr = "000";
-		std::string by = std::to_string(ball.ypos);
-		size_t diffy = ballyStr.length() - by.length();
-		for (size_t i = 0; i < by.length(); i++)
-		{
-			ballyStr.at(i + diffy) = by.at(i);
-		}
+		//std::string ballyStr = "000";
+		//std::string by = std::to_string(ball.ypos);
+		//size_t diffy = ballyStr.length() - by.length();
+		//for (size_t i = 0; i < by.length(); i++)
+		//{
+		//	ballyStr.at(i + diffy) = by.at(i);
+		//}
 
 
 		// package updated data into message string and add it to the write buffer
 
 
-		int sentBytes = 0;
-		char writeBuffer[14] = { tmp[0],tmp[1],tmp[2],tmp2[0],tmp2[1],tmp2[2],ballxStr[0],ballxStr[1],ballxStr[2],ballxStr[3],ballyStr[0],ballyStr[1],ballyStr[2],'\0'};
-	    sentBytes = send(clientPipeSocket, writeBuffer, 14, 0);
-		if (sentBytes == SOCKET_ERROR)
-		{
-			//error
-			goto loopend;
+		//int sentBytes = 0;
+		//char writeBuffer[14] = "0000000000000";// { tmp[0],tmp[1],tmp[2],tmp2[0],tmp2[1],tmp2[2],ballxStr[0],ballxStr[1],ballxStr[2],ballxStr[3],ballyStr[0],ballyStr[1],ballyStr[2],'\0'};
+	 //   sentBytes = send(clientPipeSocket, writeBuffer, 14, 0);
+		//if (sentBytes == SOCKET_ERROR)
+		//{
+		//	//error
+		//	goto loopend;
 
-		}
+		//}
 
-		int sentBytes2 = 0;
-		char writeBuffer2[14] = { tmp2[0],tmp2[1],tmp2[2],tmp[0],tmp[1],tmp[2],ballxStr[0],ballxStr[1],ballxStr[2],ballxStr[3],ballyStr[0],ballyStr[1],ballyStr[2],'\0' };
-		sentBytes2 = send(clientPipeSocket2, writeBuffer2, 14, 0);
-		if (sentBytes2 == SOCKET_ERROR)
-		{
-			//error
-			goto loopend;
+		//int sentBytes2 = 0;
+		//char writeBuffer2[14] = "0000000000000";  //{ tmp2[0],tmp2[1],tmp2[2],tmp[0],tmp[1],tmp[2],ballxStr[0],ballxStr[1],ballxStr[2],ballxStr[3],ballyStr[0],ballyStr[1],ballyStr[2],'\0' };
+		//sentBytes2 = send(clientPipeSocket2, writeBuffer2, 14, 0);
+		//if (sentBytes2 == SOCKET_ERROR)
+		//{
+		//	//error
+		//	goto loopend;
 
-		}
+		//}
 
 	}
 	loopend:
@@ -308,53 +333,144 @@ void update()
 {
 	
 
-	ball.xpos += (int)((float)(ballDirX * xSpeed));
-	ball.ypos += (int)((float)(ballDirY * ySpeed));
+	//ball.xpos += (int)((float)(ballDirX * xSpeed));
+	//ball.ypos += (int)((float)(ballDirY * ySpeed));
 
-	if (ball.xpos < 0)
-	{
-		std::cout << "Player2 Scored" << std::endl;
-		system("Pause");
-		ball.xpos = 785;
-		ball.ypos = 435;
-	}
-	if (ball.xpos >= 1600 - 30)
-	{
-		std::cout << "Player1 Scored" << std::endl;
-		system("Pause");
-		ball.xpos = 785;
-		ball.ypos = 435;
-	}
-	if (ball.ypos <= 0)
-	{
-		ballDirY = -ballDirY;
-	}
-	if (ball.ypos >= 900 - 30)
-	{
-		ballDirY = -ballDirY;
-	}
+	//if (ball.xpos < 0)
+	//{
+	//	std::cout << "Player2 Scored" << std::endl;
+	//	system("Pause");
+	//	ball.xpos = 785;
+	//	ball.ypos = 435;
+	//}
+	//if (ball.xpos >= 1600 - 30)
+	//{
+	//	std::cout << "Player1 Scored" << std::endl;
+	//	system("Pause");
+	//	ball.xpos = 785;
+	//	ball.ypos = 435;
+	//}
+	//if (ball.ypos <= 0)
+	//{
+	//	ballDirY = -ballDirY;
+	//}
+	//if (ball.ypos >= 900 - 30)
+	//{
+	//	ballDirY = -ballDirY;
+	//}
 
-	if (ball.xpos < 100)
-	{
-		if (ball.xpos < player1.xpos + 30 && ball.xpos + 30 > player1.xpos &&
-			ball.ypos < player1.ypos + 140 && ball.ypos + 30 > player1.ypos)
-		{
-			//collided
-			ballDirX = -ballDirX;
-			
-		}
-	}
+	//if (ball.xpos < 100)
+	//{
+	//	if (ball.xpos < player1.xpos + 30 && ball.xpos + 30 > player1.xpos &&
+	//		ball.ypos < player1.ypos + 140 && ball.ypos + 30 > player1.ypos)
+	//	{
+	//		//collided
+	//		ballDirX = -ballDirX;
+	//		
+	//	}
+	//}
 
-	if (ball.xpos > 1300)
-	{
-		if (ball.xpos < player2.xpos + 30 && ball.xpos + 30 > player2.xpos &&
-			ball.ypos < player2.ypos + 140 && ball.ypos + 30 > player2.ypos)
-		{
-			//collided
-			ballDirX = -ballDirX;
-			
-		}
-	}
+	//if (ball.xpos > 1300)
+	//{
+	//	if (ball.xpos < player2.xpos + 30 && ball.xpos + 30 > player2.xpos &&
+	//		ball.ypos < player2.ypos + 140 && ball.ypos + 30 > player2.ypos)
+	//	{
+	//		//collided
+	//		ballDirX = -ballDirX;
+	//		
+	//	}
+	//}
 
 	
 }
+
+
+//
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+//#include <iostream>
+//
+//int main() {
+//	WSADATA wsaData;
+//	int wsaerr;
+//	WORD wVersionRequested = MAKEWORD(2, 2);
+//	wsaerr = WSAStartup(wVersionRequested, &wsaData);
+//	if (wsaerr != 0) {
+//		std::cout << "The winsock dll not found!" << std::endl;
+//		return 0;
+//	}
+//	else {
+//		std::cout << "The winsock dll was found!" << "\n" << "The status: " << wsaData.szSystemStatus << std::endl;
+//	}
+//
+//	// set up the socket configuration
+//	SOCKET listener = INVALID_SOCKET;
+//	listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+//	if (listener == INVALID_SOCKET) {
+//		std::cout << "Error at socket(); " << WSAGetLastError() << std::endl;
+//		WSACleanup();
+//		return 0;
+//	}
+//	else {
+//		std::cout << "socket() is OK!" << std::endl;
+//	}
+//
+//	// Get the hostname of the current machine
+//	char hostname[256];
+//	if (gethostname(hostname, sizeof(hostname)) != 0) {
+//		std::cout << "gethostname failed" << std::endl;
+//		WSACleanup();
+//		return 0;
+//	}
+//
+//	// Get the IP address of the current machine
+//	struct addrinfo hints, * result, * ptr;
+//	ZeroMemory(&hints, sizeof(hints));
+//	hints.ai_family = AF_INET;
+//	hints.ai_socktype = SOCK_STREAM;
+//	hints.ai_flags = AI_PASSIVE;
+//
+//	if (getaddrinfo(hostname, NULL, &hints, &result) != 0) {
+//		std::cout << "getaddrinfo failed" << std::endl;
+//		WSACleanup();
+//		return 0;
+//	}
+//
+//	sockaddr_in service;
+//	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+//		if (ptr->ai_family == AF_INET) {
+//			service.sin_family = AF_INET;
+//			service.sin_addr = ((struct sockaddr_in*)ptr->ai_addr)->sin_addr;
+//			service.sin_port = htons(gPort);
+//			break;
+//		}
+//	}
+//
+//	freeaddrinfo(result);
+//
+//	// bind the specifics for the socket with its specific configuration
+//	if (bind(listener, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+//		std::cout << "bind failed on listener" << WSAGetLastError() << std::endl;
+//		closesocket(listener);
+//		WSACleanup();
+//		return 0;
+//	}
+//	else {
+//		std::cout << "Socket bound!" << std::endl;
+//	}
+//
+//	if (listen(listener, 8) == SOCKET_ERROR) {
+//		std::cout << "listen(): Error listening on socket! " << WSAGetLastError() << std::endl;
+//		closesocket(listener);
+//		WSACleanup();
+//		return 0;
+//	}
+//	else {
+//		std::cout << "Socket now listening for incoming requests on port: " << gPort << std::endl;
+//	}
+//
+//	std::cout << "Waiting for client connection" << std::endl;
+//
+//	WSACleanup();
+//	return 0;
+//}
